b821cb7c18b95be4a849724cf78486fb7719ff02
Ayush Bansal <bayush611@gmail.com>
2021-02-16 01:49:30 +0530

    Final Commit

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -3,7 +3,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Github and Jenkins")
+		System.out.println("Hello Github and Jenkins");
+		
 
 	}
 
